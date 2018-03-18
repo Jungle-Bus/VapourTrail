b1b0a42a58cb615c101970b7e67d2295301f4917
Pascal Rhod <pascal.rhod@canaltp.fr>
2018-03-18 09:19:18 +0100

    docker - wait for postgres in importer

---
+++ settings/run_importer.sh
@@ -0,0 +1,14 @@
+
+RETRIES=40
+
+export PGPASSWORD=$POSTGRES_PASSWORD
+
+psql -h $POSTGRES_HOST -U $POSTGRES_USER -d $POSTGRES_DB -c "select 1"
+
+until psql -h $POSTGRES_HOST -U $POSTGRES_USER -d $POSTGRES_DB -c "select 1" > /dev/null 2>&1 || [ $RETRIES -eq 0 ]; do
+ echo "Waiting for postgres server, $((RETRIES--)) remaining attempts..."
+ psql -h $POSTGRES_HOST -U $POSTGRES_USER -d $POSTGRES_DB -c "select 1"
+ sleep 1
+done
+
+./import_osm.sh
