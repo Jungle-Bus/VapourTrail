9114a5de39c0e2679b4dae9c3432aaac41758ead
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2018-06-15 21:45:10 +0200

    Make static scripts praticable

--- scripts/generate_all_tiles.sh
+++ scripts/generate_all_tiles.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
+if [ -z "$bbox" ]; then
+    echo "You need to add the bounding box as a parameter"
+    echo "   format: minlon,minlat,maxlon,maxlat"
+    echo "   example: ${0##*/} bbox=7.40701675415039,43.7229786663231,7.4437522888183585,43.7541091221655"
+    exit 1
+fi
+
 set -o errexit
 set -o pipefail
 set -o nounset
 #set -x
 
-if [ -z "$bbox" ]; then
-    echo "You need to add the bounding box as a parameter"
-    echo "   format: minlon,minlat,maxlon,maxlat"
-    echo "   example:'7.40701675415039,43.7229786663231,7.4437522888183585,43.7541091221655'"
-    exit 1
-fi
-
-docker-compose run --rm --entrypoint "t_rex generate --config /config/config.toml --overwrite true --extent=${bbox} --maxzoom 14 --minzoom 10" t-rex
+docker-compose run --rm --entrypoint "t_rex generate --config /config/config.toml --overwrite true --extent=${bbox} --maxzoom 16 --minzoom 0" t-rex
--- scripts/prepare_static.sh
+++ scripts/prepare_static.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
-set -o errexit
-set -o pipefail
-set -o nounset
-#set -x
 
 if [ -z "${static_url}" ]; then
     echo "You need to add the url of your static server as a parameter"
-    echo "   example:http://localhost:8080/tiles"
+    echo "   example: ${0##*/} static_url=http://localhost:8080/tiles"
     exit 1
 fi
+
+set -o errexit
+set -o pipefail
+set -o nounset
+set -x
 
 # init directory
 rm -fr static
@@ -20,6 +21,7 @@ cp -r web/* static/
 
 # copy dumped tiles
 cp -r docker/trex_cache/ static/tiles
+find static/tiles -name "*.pbf" -size `du -b static/tiles/vapour_trail/0/0/0.pbf | cut -f 1`c -delete
 find static/tiles -type d -empty -delete
 find static/tiles -name "*.pbf" -exec mv {} {}.gz \;
 find static/tiles -name "*.pbf.gz" -exec gzip -d {} \;
