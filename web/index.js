3f8e586fc38ef311225f2586c2f927d772b1bc59
Noémie <noemie.lehuby@zaclys.net>
2018-08-18 13:46:15 +0200

    panel with route detail and stop list using the API

--- web/index.js
+++ web/index.js
@@ -6,13 +6,15 @@ var tactile_img = "<img src='img/tactile.svg' heigth='20px' width='20px' />"
 var bench_img = "<img src='img/bench.svg' heigth='20px' width='20px' />"
 var departures_img = "<img src='img/departures.svg' heigth='20px' width='20px' />"
 
+const vapour_trail_api_base_url = "http://localhost:5000";
+
 var map = new mapboxgl.Map({
     container: 'map',
     style: 'glstyle.json',
     center: [
         1.8659, 46.1662
     ],
-    zoom: 11.952145030855498,
+    zoom: 13,
     hash: true
 });
 map.on('load', function() {
@@ -22,6 +24,34 @@ map.on('load', function() {
     });
     map.on('mouseleave', 'stops-icon', function() {
         map.getCanvas().style.cursor = '';
+    });
+
+    //new layer to highlight a stop in the stop_list
+    map.addSource('highlight_stop_source', {
+        "type": "geojson",
+        "data": {
+            "type": "Point",
+            "coordinates": [1.8790708, 46.1740393]
+        }
+    });
+    map.addLayer({
+        "id": "highlighted_stop",
+        "source": "highlight_stop_source",
+        "type": "circle",
+        "layout": {
+          "visibility": "none",
+        },
+        'paint': {
+            'circle-radius': {
+                'base': 1.75,
+                'stops': [
+                    [12, 10],
+                    [22, 200]
+                ]
+            },
+            'circle-color': '#6e6b6b',
+            'circle-opacity': 0.5
+        }
     });
 
     function on_stop(e) {
@@ -77,35 +107,130 @@ map.on('load', function() {
 
 
 function filter_on_one_route(route) {
-    var ways_ids = route['ways_ids'];
-    var positions_ids = route['positions_ids'];
-    map.setFilter('routes_ways_filtered_outline', ["in", "id"].concat(ways_ids || []));
-    map.setFilter('routes_ways_filtered', ["in", "id"].concat(ways_ids || []));
-    map.setFilter('routes_points_filtered', ["in", "id"].concat(positions_ids || []));
-    map.setPaintProperty('routes_ways_filtered', "line-color", route['rel_colour'] || 'grey')
-    map.setPaintProperty('routes_points_filtered', "circle-color", route['rel_colour'] || 'grey')
-    html = `<div class='bus_box_div'>
-                <span class='bus_box' style='border-bottom-color: ${route['rel_colour'] || "grey"};' >
-                    <span>🚍</span>
-                    <span>${route['rel_ref'] || '??'}</span>
-                </span>
-                &nbsp; ${route['rel_name']}
-            </div>`
-    html += `De <b>${route['rel_origin'] || '??'}</b> vers <b>${route['rel_destination'] || '??'}</b>`;
-    if (route.hasOwnProperty('rel_via')){
-        html += `via <b>${route['rel_via'] || '??'}</b>`;
-    }
-    html += `<br>Réseau ${route['rel_network'] || '??'}`;
-    html += `<br>Transporteur : ${route['rel_operator'] || '??'}`
-    html += `<br><a href='#' onclick='reset_filters_and_show_all_lines()'>Masquer la ligne</a> <br/>`
+    reset_filters_and_show_all_lines();
 
-    caisson.add_content(html);
+    var caisson_content = `
+    <div id="route_info"></div>
+    <div id="close_caisson_button"></div>
+    <div id="stop_list" class="stop_list"></div>
+    <div id="osm_attribution"></div>
+    `
+    caisson.add_content(caisson_content)
+    var close_caisson_button = document.getElementById('close_caisson_button')
+    close_caisson_button.innerHTML = `<a href='#' onclick='reset_filters_and_show_all_lines()'>Masquer la ligne</a>`;
+
+    const route_id = route['rel_osm_id'];
+
+    const stop_list_url = vapour_trail_api_base_url + "/routes/" + route_id;
+    fetch(stop_list_url)
+        .then(function(data) {
+            return data.json()
+        })
+        .then(function(route_data) {
+            var route_colour = route_data['route_info']['colour'] || 'grey';
+            var route_info = document.getElementById('route_info');
+            route_info.innerHTML = create_route_medata(route_data['route_info']);
+
+            var thermo = create_stop_list_for_a_route(route_data['stop_list'], route_colour)
+            var stop_list = document.getElementById('stop_list')
+            stop_list.innerHTML = thermo;
+
+            var osm_attribution = document.getElementById('osm_attribution')
+            osm_attribution.innerHTML = create_osm_attribution_for_the_route(route_data['route_info']['osm_id']);
+
+
+            var route_geom = route_data['geometry'];
+            map.getSource('route_filter').setData(route_geom);
+            map.setFilter('routes_ways_filtered', ["all"]);
+            map.setFilter('routes_ways_filtered_outline', ["all"]);
+            map.setPaintProperty('routes_ways_filtered', "line-color", route_colour)
+
+            var positions_geom = route_data['stop_positions'];
+            map.getSource('route_positions_filter').setData(positions_geom);
+            map.setFilter('routes_points_filtered', ["all"]);
+            map.setPaintProperty('routes_points_filtered', "circle-color", route_colour)
+
+        })
+        .catch(function(error) {
+            console.log("erreur en récupérant les informations sur la ligne : " + error)
+        })
+
 };
 
+function create_osm_attribution_for_the_route(osm_route_id) {
+    var inner_html = `<small>Ces informations proviennent d'<a href='https://OpenStreetMap.org' target='_blank'>OpenStreetMap</a>, la carte libre et collaborative.
+    Rejoignez la communauté pour compléter ou corriger le détail de <a href='https://OpenStreetMap.org/relation/${osm_route_id}' target='_blank'>cette ligne</a> !</small>`;
+    return inner_html
+}
+
+function create_route_medata(route_info) {
+    var inner_html = `<div class='bus_box_div'>
+                <span class='bus_box' style='border-bottom-color: ${route_info['colour'] || "grey"};' >
+                    <span>🚍</span>
+                    <span>${route_info['ref'] || '??'}</span>
+                </span>
+                &nbsp; ${route_info['name']}
+            </div>`;
+    inner_html += `De <b>${route_info['origin'] || '??'}</b> vers <b>${route_info['destination'] || '??'}</b>`;
+    if (route_info.hasOwnProperty('via')) {
+        inner_html += `via <b>${route_info['via'] || '??'}</b>`;
+    };
+    inner_html += `<br>Réseau ${route_info['network'] || '??'}`;
+    inner_html += `<br>Transporteur : ${route_info['operator'] || '??'}`;
+    return inner_html;
+}
+
+function create_stop_list_for_a_route(stop_list, route_colour) {
+    var route_colour = route_colour || 'grey';
+    var inner_html = ''
+    for (const stop of stop_list) {
+        if (stop != stop_list[stop_list.length - 1]) {
+            var border_color = route_colour;
+        } else { // remove the border so the stop list stops on a dot
+            var border_color = "#FFF";
+        }
+
+        inner_html += `<div class="stop_item" style="border-left-color:${border_color};"
+                            onmouseover="highlight_one_stop(${stop['lon']}, ${stop['lat']})"
+                            onclick="map.flyTo({center: [${stop['lon']}, ${stop['lat']}]});">`;
+
+        inner_html += `
+          <span class="stop_dot" style="border-color:${route_colour};"></span>
+          <div class="stop_name">${stop['name'] || '??'}</div>
+          <div class="stop_shields">
+          `
+        for (const shield of stop['shields']) {
+            inner_html += `
+                <div class='bus_box_inline_div'>
+                  <span class='bus_box' style='border-bottom-color: ${shield['colour'] || "grey"};' >
+                    <span>🚍</span>
+                  <span>${shield['ref'] || '??'}</span>
+                  </span>
+                </div>
+                `
+        }
+        inner_html += `
+          </div>
+        </div>
+        `
+    }
+
+    return inner_html
+};
 
 function reset_filters_and_show_all_lines() {
     map.setFilter('routes_ways_filtered_outline', ["==", "rel_osm_id", "dumb_filter_again"]);
     map.setFilter('routes_ways_filtered', ["==", "rel_osm_id", "dumb_filter_again"]);
     map.setFilter('routes_points_filtered', ["==", "rel_osm_id", "dumb_filter_again"]);
     caisson.remove()
-}
+};
+
+function highlight_one_stop(stop_lon, stop_lat) {
+ map.getSource('highlight_stop_source').setData(
+     {
+         "type": "Point",
+         "coordinates": [stop_lon, stop_lat]
+     }
+ );
+ map.setLayoutProperty('highlighted_stop', 'visibility', 'visible');
+};
